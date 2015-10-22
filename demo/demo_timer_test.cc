c20d136eefd7dcfa4bc0314b81a555df37685b12
Sam Yates <sam@quux.dropbear.id.au>
2015-10-22 13:18:00 -0400

    Add generic timer interface code.

---
+++ demo/demo_timer_test.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <thread>
+
+#include "rdmini/timer.h"
+
+void sleep(double dt) {
+    std::this_thread::sleep_for(std::chrono::duration<double>(dt));
+}
+
+int main() {
+    timer::hr_timer T;
+
+    std::cout << "T.time()=" << T.time() << "\n";
+    T.start();
+    sleep(2.3);
+    T.stop();
+    std::cout << "After sleep(2.3): T.time()=" << T.time() << "\n";
+    T.resume();
+    sleep(3.5);
+    T.stop();
+    std::cout << "After sleep(3.5): T.time()=" << T.time() << "\n";
+    T.reset();
+    std::cout << "Reset timer.\n";
+    {
+        auto _=timer::guard(T);
+        sleep(4.8);
+    }
+    std::cout << "After sleep(4.8) (using guard): T.time()=" << T.time() << "\n";
+}
+
